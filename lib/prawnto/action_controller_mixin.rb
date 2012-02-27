deb8062eb6f65788a693e32d1e383d8941121bda
bondarev <alexander.i.bondarev@gmail.com>
2012-02-27 13:39:45 +0400

    Fixes for compatibility with Rails 3.1. Bump version to 0.1.0

--- lib/prawnto/action_controller_mixin.rb
+++ lib/prawnto/action_controller_mixin.rb
@@ -1,20 +1,44 @@
+require "prawnto/available_features"
+
 module Prawnto
   module ActionControllerMixin
+    DEFAULT_PRAWNTO_OPTIONS = {:inline => true}
 
-    DEFAULT_PRAWNTO_OPTIONS = {:inline=>true}
-      
     def self.included(base)
       base.extend ClassMethods
     end
 
     module ClassMethods
+      if class_attribute_supported?
+        def self.extended(cls)
+          cls.send(:class_attribute, :prawn_options)
+          cls.send(:class_attribute, :prawnto_options)
+        end
+      else
+        def prawn_options=(options)
+          write_inheritable_hash(:prawn_options, options)
+        end
+
+        def prawn_options
+          read_inheritable_attribute(:prawn_options)
+        end
+
+        def prawnto_options=(options)
+          write_inheritable_hash(:prawnto_options, options)
+        end
+
+        def prawnto_options
+          read_inheritable_attribute(:prawnto_options)
+        end
+      end
+
       def prawnto(options)
         prawn_options, prawnto_options = breakdown_prawnto_options options
-        write_inheritable_hash(:prawn, prawn_options)
-        write_inheritable_hash(:prawnto, prawnto_options)
+        self.prawn_options = prawn_options
+        self.prawnto_options = prawnto_options
       end
-    
-    private
+
+      private
 
       def breakdown_prawnto_options(options)
         prawnto_options = options.dup
@@ -28,17 +52,15 @@ module Prawnto
       @prawnto_options.merge! options
     end
 
-
-  private
+    private
 
     def compute_prawnto_options
       @prawnto_options ||= DEFAULT_PRAWNTO_OPTIONS.dup
       @prawnto_options[:prawn] ||= {}
-      @prawnto_options[:prawn].merge!(self.class.read_inheritable_attribute(:prawn) || {}) {|k,o,n| o}
-      @prawnto_options.merge!(self.class.read_inheritable_attribute(:prawnto) || {}) {|k,o,n| o}
+      @prawnto_options[:prawn].merge!(self.class.prawn_options || {}) { |k, o, n| o }
+      @prawnto_options.merge!(self.class.prawnto_options || {}) { |k, o, n| o }
       @prawnto_options
     end
-
   end
 end
 
--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -1,7 +1,9 @@
+require "prawnto/available_features"
+
 module Prawnto
   module TemplateHandlers
-    class Base < (::Rails::VERSION::MAJOR < 3 ? ::ActionView::TemplateHandler : ::Object)
-      include ::ActionView::TemplateHandlers::Compilable if ::Rails::VERSION::MAJOR < 3
+    class Base < (base_class_for_template_handler_required? ? ::ActionView::TemplateHandler : ::Object)
+      include ::ActionView::TemplateHandlers::Compilable if template_should_include_compilable?
 
       def self.call(template)
         "_prawnto_compile_setup;" +
@@ -10,7 +12,7 @@ module Prawnto
         "pdf.render;"
       end
 
-      if ::Rails::VERSION::MAJOR < 3 || (::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MINOR < 1)
+      unless template_has_class_level_call_method?
         def compile(template)
           self.class.call(template)
         end
